f4a53fad354c505cbb19cdaf7204a0b2a1becae1
Maxim Kondratenko <max.kondr@gmail.com>
2025-12-04 16:11:16 +0200

    Update

--- internal/webhook/everest/v1alpha1/databaseclusterbackup_webhook.go
+++ internal/webhook/everest/v1alpha1/databaseclusterbackup_webhook.go
@@ -62,7 +62,7 @@ func SetupDatabaseClusterBackupWebhookWithManager(mgr ctrl.Manager) error {
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
 //nolint:lll
-// +kubebuilder:webhook:path=/validate-everest-percona-com-v1alpha1-databaseclusterbackup,mutating=false,failurePolicy=fail,sideEffects=None,groups=everest.percona.com,resources=databaseclusterbackups,verbs=create;update,versions=v1alpha1,name=vdatabaseclusterbackup-v1alpha1.everest.percona.com,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-everest-percona-com-v1alpha1-databaseclusterbackup,mutating=false,failurePolicy=fail,sideEffects=None,groups=everest.percona.com,resources=databaseclusterbackups,verbs=create;update;delete,versions=v1alpha1,name=vdatabaseclusterbackup-v1alpha1.everest.percona.com,admissionReviewVersions=v1
 
 // DatabaseClusterBackupCustomValidator struct is responsible for validating the DatabaseClusterBackup resource
 // when it is created, updated, or deleted.
